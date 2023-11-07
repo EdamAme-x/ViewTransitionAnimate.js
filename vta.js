ccb99cffed0a07fe4c384fbe413b8cbac8b9c6af
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-11-07 15:28:55 +0900

    完成

--- vta.js
+++ vta.js
@@ -1,3 +1,21 @@
+function onVtaReload() {
+   document.head.innerHTML += `
+    <style>
+        .vta-route-announcer {
+            position: absolute;
+            left: 0;
+            top: 0;
+            clip: rect(0 0 0 0);
+            clip-path: inset(50%);
+            overflow: hidden;
+            white-space: nowrap;
+            width: 1px;
+            height: 1px
+        }
+    </style>
+    `;
+}
+
 const $ = (t) =>
     history.state &&
     history.replaceState(
@@ -5,28 +23,29 @@ const $ = (t) =>
         ...history.state,
         ...t,
       },
-      ''
+      ""
     ),
   g = !!document.startViewTransition,
-  E = () => !!document.querySelector('[name="vta-view-transitions-enabled"]'),
+  E = () => true,
   S = (t) => location.pathname === t.pathname && location.search === t.search,
   k = (t) => document.dispatchEvent(new Event(t)),
-  x = () => k('astro:page-load'),
+  x = () => k("on-vta-load"),
   H = () => {
-    let t = document.createElement('div');
-    t.setAttribute('aria-live', 'assertive'),
-      t.setAttribute('aria-atomic', 'true'),
-      (t.className = 'vta-route-announcer'),
+    let t = document.createElement("div");
+    t.setAttribute("aria-live", "assertive"),
+      t.setAttribute("aria-atomic", "true"),
+      (t.className = "vta-route-announcer"),
       document.body.append(t),
       setTimeout(() => {
         let e =
           document.title ||
-          document.querySelector('h1')?.textContent ||
+          document.querySelector("h1")?.textContent ||
           location.pathname;
         t.textContent = e;
+        onVtaReload()
       }, 60);
   },
-  d = 'data-vta-transition-persist';
+  d = "data-vta-transition-persist";
 let T,
   y = 0;
 history.state
@@ -43,7 +62,7 @@ history.state
         scrollY,
         intraPage: !1,
       },
-      ''
+      ""
     );
 const M = (t, e) => {
   let r = !1,
@@ -63,8 +82,8 @@ const M = (t, e) => {
 async function R(t) {
   try {
     const e = await fetch(t),
-      r = e.headers.get('content-type')?.replace(/;.*$/, '');
-    return r !== 'text/html' && r !== 'application/xhtml+xml'
+      r = e.headers.get("content-type")?.replace(/;.*$/, "");
+    return r !== "text/html" && r !== "application/xhtml+xml"
       ? null
       : {
           html: await e.text(),
@@ -76,17 +95,16 @@ async function R(t) {
   }
 }
 function P() {
-  const t = document.querySelector('[name="vta-view-transitions-fallback"]');
-  return t ? t.getAttribute('content') : 'animate';
+  return "animate";
 }
 function F() {
   let t = Promise.resolve();
   for (const e of Array.from(document.scripts)) {
-    if (e.dataset.astroExec === '') continue;
-    const r = document.createElement('script');
+    if (e.dataset.vtaExec === "") continue;
+    const r = document.createElement("script");
     r.innerHTML = e.innerHTML;
     for (const n of e.attributes) {
-      if (n.name === 'src') {
+      if (n.name === "src") {
         const i = new Promise((c) => {
           r.onload = c;
         });
@@ -94,7 +112,7 @@ function F() {
       }
       r.setAttribute(n.name, n.value);
     }
-    (r.dataset.astroExec = ''), e.replaceWith(r);
+    (r.dataset.vtaExec = ""), e.replaceWith(r);
   }
   return t;
 }
@@ -103,7 +121,7 @@ function I(t) {
   return !e || !(e instanceof KeyframeEffect) || !e.target
     ? !1
     : window.getComputedStyle(e.target, e.pseudoElement)
-        .animationIterationCount === 'infinite';
+        .animationIterationCount === "infinite";
 }
 const L = (t, e, r) => {
   const n = !S(t);
@@ -114,7 +132,7 @@ const L = (t, e, r) => {
           {
             ...history.state,
           },
-          '',
+          "",
           t.href
         )
       : (history.replaceState(
@@ -122,7 +140,7 @@ const L = (t, e, r) => {
             ...history.state,
             intraPage: r,
           },
-          ''
+          ""
         ),
         history.pushState(
           {
@@ -130,14 +148,14 @@ const L = (t, e, r) => {
             scrollX: 0,
             scrollY: 0,
           },
-          '',
+          "",
           t.href
         )),
     n &&
       (scrollTo({
         left: 0,
         top: 0,
-        behavior: 'instant',
+        behavior: "instant",
       }),
       (i = !0))),
     t.hash
@@ -146,26 +164,26 @@ const L = (t, e, r) => {
         scrollTo({
           left: 0,
           top: 0,
-          behavior: 'instant',
+          behavior: "instant",
         });
 };
 function X(t) {
   const e = [];
-  for (const r of t.querySelectorAll('head link[rel=stylesheet]'))
+  for (const r of t.querySelectorAll("head link[rel=stylesheet]"))
     if (
       !document.querySelector(
         `[${d}="${r.getAttribute(
           d
-        )}"], link[rel=stylesheet][href="${r.getAttribute('href')}"]`
+        )}"], link[rel=stylesheet][href="${r.getAttribute("href")}"]`
       )
     ) {
-      const n = document.createElement('link');
-      n.setAttribute('rel', 'preload'),
-        n.setAttribute('as', 'style'),
-        n.setAttribute('href', r.getAttribute('href')),
+      const n = document.createElement("link");
+      n.setAttribute("rel", "preload"),
+        n.setAttribute("as", "style"),
+        n.setAttribute("href", r.getAttribute("href")),
         e.push(
           new Promise((i) => {
-            ['load', 'error'].forEach((c) => n.addEventListener(c, i)),
+            ["load", "error"].forEach((c) => n.addEventListener(c, i)),
               document.head.append(n);
           })
         );
@@ -177,8 +195,8 @@ async function w(t, e, r, n, i) {
       const l = o.getAttribute(d),
         f = l && t.head.querySelector(`[${d}="${l}"]`);
       if (f) return f;
-      if (o.matches('link[rel=stylesheet]')) {
-        const u = o.getAttribute('href');
+      if (o.matches("link[rel=stylesheet]")) {
+        const u = o.getAttribute("href");
         return t.head.querySelector(`link[rel=stylesheet][href="${u}"]`);
       }
       return null;
@@ -212,7 +230,7 @@ async function w(t, e, r, n, i) {
     p = () => {
       const o = document.documentElement,
         l = [...o.attributes].filter(
-          ({ name: s }) => (o.removeAttribute(s), s.startsWith('data-vta-'))
+          ({ name: s }) => (o.removeAttribute(s), s.startsWith("data-vta-"))
         );
       [...t.documentElement.attributes, ...l].forEach(({ name: s, value: a }) =>
         o.setAttribute(s, a)
@@ -223,7 +241,7 @@ async function w(t, e, r, n, i) {
             (!s.src && s.textContent === a.textContent) ||
             (s.src && s.type === a.type && s.src === a.src)
           ) {
-            a.dataset.astroExec = '';
+            a.dataset.vtaExec = "";
             break;
           }
       for (const s of Array.from(document.head.children)) {
@@ -240,17 +258,17 @@ async function w(t, e, r, n, i) {
         A && A.replaceWith(s);
       }
       m(u),
-        n ? scrollTo(n.scrollX, n.scrollY) : L(e, r.history === 'replace', !1),
-        k('astro:after-swap');
+        n ? scrollTo(n.scrollX, n.scrollY) : L(e, r.history === "replace", !1),
+        k("after-vta-swap");
     },
     b = X(t);
-  if ((b.length && (await Promise.all(b)), i === 'animate')) {
+  if ((b.length && (await Promise.all(b)), i === "animate")) {
     const o = document.getAnimations();
-    document.documentElement.dataset.astroTransitionFallback = 'old';
+    document.documentElement.dataset.vtaTransitionFallback = "old";
     const l = document.getAnimations().filter((u) => !o.includes(u) && !I(u));
     await Promise.all(l.map((u) => u.finished)),
       p(),
-      (document.documentElement.dataset.astroTransitionFallback = 'new');
+      (document.documentElement.dataset.vtaTransitionFallback = "new");
   } else p();
 }
 async function q(t, e, r, n) {
@@ -263,10 +281,7 @@ async function q(t, e, r, n) {
   }
   h.redirected && (e = new URL(h.redirected)), (T ??= new DOMParser());
   const m = T.parseFromString(h.html, h.mediaType);
-  if (
-    (m.querySelectorAll('noscript').forEach((p) => p.remove()),
-    !m.querySelector('[name="vta-view-transitions-enabled"]'))
-  ) {
+  if ((m.querySelectorAll("noscript").forEach((p) => p.remove()), false)) {
     location.href = c;
     return;
   }
@@ -277,9 +292,9 @@ async function q(t, e, r, n) {
         scrollX,
         scrollY,
       },
-      ''
+      ""
     ),
-    (document.documentElement.dataset.astroTransition = t),
+    (document.documentElement.dataset.vtaTransition = t),
     g
       ? (i = document.startViewTransition(() => w(m, e, r, n)).finished)
       : (i = w(m, e, r, n, P()));
@@ -296,25 +311,25 @@ function Y(t, e) {
   }
   const r = new URL(t, location.href);
   location.origin === r.origin && S(r)
-    ? L(r, e?.history === 'replace', !0)
-    : q('forward', r, e ?? {});
+    ? L(r, e?.history === "replace", !0)
+    : q("forward", r, e ?? {});
 }
 function C(t) {
   if (!E() && t.state) {
-    history.scrollRestoration && (history.scrollRestoration = 'manual'),
+    history.scrollRestoration && (history.scrollRestoration = "manual"),
       location.reload();
     return;
   }
   if (t.state === null) {
-    history.scrollRestoration && (history.scrollRestoration = 'auto');
+    history.scrollRestoration && (history.scrollRestoration = "auto");
     return;
   }
-  history.scrollRestoration && (history.scrollRestoration = 'manual');
+  history.scrollRestoration && (history.scrollRestoration = "manual");
   const e = history.state;
   if (e.intraPage) scrollTo(e.scrollX, e.scrollY);
   else {
     const r = e.index,
-      n = r > y ? 'forward' : 'back';
+      n = r > y ? "forward" : "back";
     (y = r), q(n, new URL(location.href), {}, e);
   }
 }
@@ -325,42 +340,41 @@ const v = () => {
   });
 };
 {
-  (g || P() !== 'none') &&
-    (addEventListener('popstate', C),
-    addEventListener('load', x),
-    'onscrollend' in window
-      ? addEventListener('scrollend', v)
-      : addEventListener('scroll', M(v, 350), {
+  (g || P() !== "none") &&
+    (addEventListener("popstate", C),
+    addEventListener("load", x),
+    "onscrollend" in window
+      ? addEventListener("scrollend", v)
+      : addEventListener("scroll", M(v, 350), {
           passive: !0,
         }));
-  for (const t of document.scripts) t.dataset.astroExec = '';
+  for (const t of document.scripts) t.dataset.vtaExec = "";
 }
 function K() {
-  const t = document.querySelector('[name="vta-view-transitions-fallback"]');
-  return t ? t.getAttribute('content') : 'animate';
+  return "animate";
 }
 function W(t) {
   if (document.querySelector(`link[rel=prefetch][href="${t}"]`)) return;
   if (navigator.connection) {
     let r = navigator.connection;
-    if (r.saveData || /(2|3)g/.test(r.effectiveType || '')) return;
+    if (r.saveData || /(2|3)g/.test(r.effectiveType || "")) return;
   }
-  let e = document.createElement('link');
-  e.setAttribute('rel', 'prefetch'),
-    e.setAttribute('href', t),
+  let e = document.createElement("link");
+  e.setAttribute("rel", "prefetch"),
+    e.setAttribute("href", t),
     document.head.append(e);
 }
-(g || K() !== 'none') &&
-  (document.addEventListener('click', (t) => {
+(g || K() !== "none") &&
+  (document.addEventListener("click", (t) => {
     let e = t.target;
-    e instanceof Element && e.tagName !== 'A' && (e = e.closest('a')),
+    e instanceof Element && e.tagName !== "A" && (e = e.closest("a")),
       !(
         !e ||
         !(e instanceof HTMLAnchorElement) ||
-        e.dataset.astroReload !== void 0 ||
-        e.hasAttribute('download') ||
+        e.dataset.vtaReload !== void 0 ||
+        e.hasAttribute("download") ||
         !e.href ||
-        (e.target && e.target !== '_self') ||
+        (e.target && e.target !== "_self") ||
         e.origin !== location.origin ||
         t.button !== 0 ||
         t.metaKey ||
@@ -371,10 +385,10 @@ function W(t) {
       ) &&
         (t.preventDefault(),
         Y(e.href, {
-          history: e.dataset.astroHistory === 'replace' ? 'replace' : 'auto',
+          history: e.dataset.vtaHistory === "replace" ? "replace" : "auto",
         }));
   }),
-  ['mouseenter', 'touchstart', 'focus'].forEach((t) => {
+  ["mouseenter", "touchstart", "focus"].forEach((t) => {
     document.addEventListener(
       t,
       (e) => {
